ec6e23ab3310a5ab458bdd571db5ddae73b800ee
Swarnendu <clickswarnendu123@gmail.com>
2025-02-11 21:28:59 +0530

    minor changes

--- client/src/components/auth/login.auth.tsx
+++ client/src/components/auth/login.auth.tsx
@@ -158,7 +158,7 @@ const Auth = () => {
                       <svg
                         className="h-6 w-6 mr-2"
                         xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        
                         width="800px"
                         height="800px"
                         viewBox="-0.5 0 48 48"
