36283115d75a80bb5ee875b1bf41f6bbeb706797
Swarnendu <clickswarnendu123@gmail.com>
2025-03-14 23:39:16 +0530

    Build error fixed

--- client/src/pages/ShortURL_Redirect.tsx
+++ client/src/pages/ShortURL_Redirect.tsx
@@ -1,19 +1,18 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import Navigation from "../components/Navigation";
-import { blogs } from "../assets/data";
-import { Navigate } from "react-router-dom";
+
 
 const Redirect = () => {
-  const { id } = useParams();
-  const b = blogs.filter((blog) => blog.short_id === id)[0];
-  const redirect_id = b.id;
-  console.log(redirect_id);
+  // const { id } = useParams();
+  // const b = blogs.filter((blog) => blog.short_id === id)[0];
+  // const redirect_id = b.id;
+  // console.log(redirect_id);
 
   return (
     <div>
       <Navigation />
       <h1>Redirecting...</h1>
-      <Navigate to={`/blog/${redirect_id}`} />;
+      {/* <Navigate to={`/blog/${redirect_id}`} />; */}
     </div>
   );
 };
